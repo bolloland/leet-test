effd14d1eb22ce64e0ff707f714fcc307ac7b769
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-03-20 09:35:37 -0500

    Time: 799 ms (5.01%), Space: 52.2 MB (5.29%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -6,22 +6,30 @@
 let isValid = function(s) {
  const stack = []
  
- for (i=0; i<s.length; i++) {
+ for (i = 0; i < s.length; i++) {
    let curChar = s[i]
-   
+    console.log(curChar, "curChar")
    switch(curChar) {
      case '(': stack.push(")");
+           console.log(stack, ")")
        break;
      case '[': stack.push("]");
+           console.log(stack, "]")
        break;
      case '{': stack.push("}");
+           console.log(stack, "}")
        break;
      default:
        topElement = stack.pop()
+           console.log(stack, "after breaks or pop")///removes last element and returns array
        if (curChar !== topElement) return false  
    }
  }
   return stack.length === 0
 }
 
-
+// create a new array (STACK). for every left side, enter right side counterpart in stack
+// because it SHOULD be balanced, the first right symbol, should match the last left symbol in the STACK
+//  ex: [[()]]
+// STACK: [ "]", "]", ")" ] after the iterating thru first 3 symbols
+// now example[3] = ")" and that should == stack.pop()  {the last in the array}
