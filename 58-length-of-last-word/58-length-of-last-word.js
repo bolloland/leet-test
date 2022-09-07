cda586ab1360b5949eb9fe3b2981ed02e68a0583
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-09-06 22:33:23 -0500

    Time: 113 ms (8.95%), Space: 42.3 MB (19.37%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.js
+++ 58-length-of-last-word/58-length-of-last-word.js
@@ -4,7 +4,6 @@
  */
 var lengthOfLastWord = function(s) {
     let array = s.split(" ").filter(x => x.length > 0)
-    console.log(array)
     let last = array.splice(-1, 1)
     return last[0].length
 
