8186d8daa4c49a2db6219f40ceef9b6a6d2f471a
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-09-10 17:34:03 -0500

    Time: 252 ms (5.34%), Space: 49.6 MB (5.10%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+   for (i = nums2.length; i > 0; i-- ) {
+      nums1.unshift(nums2[0])
+      nums1.pop()
+      nums2.shift()
+      console.log(nums1)
+  }
+    nums1.sort((a, b) => a-b)
+    console.log(nums1, "sorted")
+};
+
+
+//add nums2 to front of nums1, hack off last n terms of nums1
+//and sort?  nums1.splice(n, m)
