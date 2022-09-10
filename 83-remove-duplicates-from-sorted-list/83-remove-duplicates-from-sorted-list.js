d1c803dd9e46122c600428baf5e05696fd4a2352
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-09-10 16:11:10 -0500

    Time: 110 ms (48.27%), Space: 44.6 MB (14.76%) - LeetHub

--- 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.js
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.js
@@ -11,10 +11,14 @@
  */
 var deleteDuplicates = function(head) {
     let current = head
+    //while current and next exist...
     while (current && current.next) {
+        // if the values of one and the next are the same
         if (current.val === current.next.val) {
+            // skip the first duplicate, and make the "next", the next one in order
             current.next = current.next.next
         } else {
+            //if not the same, go to the next one in order and test that
             current = current.next
         }
     }
