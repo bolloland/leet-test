b2170db8bee43a735f0b8cd22c81d09472e037d7
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-11-29 00:22:54 -0600

    Time: 62 ms (95.97%), Space: 42.6 MB (27.75%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -3,18 +3,11 @@
  * @return {number[]}
  */
 var runningSum = function(nums) {
-    console.log(nums)
-    // nums is an array
-    //  for each, nums[i] = nums [i] + all that came before it. 
     if (nums.length == 1 || nums.length == 0) return nums
     let solut = []
-    // nums.length == 3 // [2, 3, 4]
-    
+     
     for (let i = 1; i < nums.length; i++) {
         solut[0] = nums[0]
-        console.log(solut, "solut")
-        // let sumArray = solut.reduce((a, b) => a+b)
-        // console.log(sumArray, "sumArray")
         solut[i] = nums[i] + solut[i-1] 
         }
     return solut
