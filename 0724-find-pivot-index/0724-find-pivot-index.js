76e7dec3b86870642c3e4f671e6a53f96664dd34
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-11-30 23:21:34 -0600

    Time: 1647 ms (10.26%), Space: 56.6 MB (6.86%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.js
+++ 0724-find-pivot-index/0724-find-pivot-index.js
@@ -3,13 +3,13 @@
  * @return {number}
  */
 var pivotIndex = function(nums) {
-    let leftSum = 0
-    let rightSum = 0
+    // let leftSum = 0
+    // let rightSum = 0
     // let pivot = -1
 for(let i = 0; i < nums.length; i++) {
-    leftSum = nums.slice(0, i).reduce((a,b)=>(a+b),0)
+    let leftSum = nums.slice(0, i).reduce((a,b)=>(a+b),0)
     console.log(leftSum, "left sum")
-    rightSum = nums.slice(i+1, nums.length).reduce((a,b)=>(a+b),0)
+    let rightSum = nums.slice(i+1, nums.length).reduce((a,b)=>(a+b),0)
     console.log(rightSum,"right sum")
     if (leftSum == rightSum) return i
     }
