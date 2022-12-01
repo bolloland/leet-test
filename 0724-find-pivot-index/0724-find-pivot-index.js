a39759455a200a228b02f14686b7e6ef9fe2e9d9
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-11-30 23:22:06 -0600

    Time: 1064 ms (14.08%), Space: 48.7 MB (16.00%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.js
+++ 0724-find-pivot-index/0724-find-pivot-index.js
@@ -3,14 +3,9 @@
  * @return {number}
  */
 var pivotIndex = function(nums) {
-    // let leftSum = 0
-    // let rightSum = 0
-    // let pivot = -1
 for(let i = 0; i < nums.length; i++) {
     let leftSum = nums.slice(0, i).reduce((a,b)=>(a+b),0)
-    console.log(leftSum, "left sum")
     let rightSum = nums.slice(i+1, nums.length).reduce((a,b)=>(a+b),0)
-    console.log(rightSum,"right sum")
     if (leftSum == rightSum) return i
     }
     return -1
